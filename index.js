f16dd1681c7520274b87c9dc51368c014493798d
xushuo <893804934@qq.com>
2017-03-06 12:30:11 +0800

    完成 爬取豆瓣以及北邮人论坛的每日十大的信息

---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+var cheerio = require('cheerio');
+var superagent = require('superagent-charset');
+var iconv = require('iconv-lite');
+var request = require('superagent');
+var fs = require('fs');
+var file='topten.js';
+var url = 'http://bbs.byr.cn/rss/topten';
+var toptens = [];//初始化json数组
+superagent(request);
+request.get(url) //获取网页内容
+    .charset('gb2312') //转码-gb2312格式转成utf-8
+    .end(function(err,res){
+        //常规的错误处理
+        if(err){
+            return next(err);
+        }
+        var $ = cheerio.load(res.text,{
+            xmlMode:true //由于从rss里读取xml，所以这一步一定要用
+        });
+        var d =new Date();
+        var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+(d.getDate());//取得爬取的日期
+        var topten ={ //设定爬取的json数组
+            date:date,
+            info:[]
+        };
+        //具体爬取内容，主要都是cheeio操作
+        $('item').each(function(i,el){
+            i+=1;
+            topten.info.push({
+                topno:i,
+                title:$(this).find('title').text(),
+                author:$(this).find('author').text(),
+                pubDate:$(this).find('purDate').text(),
+                boardName:$(this).find('guid').text().replace(/http:\/\/bbs.byr.cn\/article\//,'').replace(/\/\d+/,'').trim(),
+                link:$(this).find('link').text(),
+                content:$(this).find('description').text()
+            });
+        });
+        toptens.unshift({topten:topten});//从原有的json数据之前追加json数据
+        var json = JSON.stringify(toptens);//从json格式解析
+        fs.writeFile(file,json,'utf-8',function(err){
+            if(err){
+                throw err
+            }else {
+                console.log('JSON写入成功\r\n'+json);
+            }
+        })
+    })
--- server.js
+++ server.js
@@ -6,35 +6,81 @@
 var cheerio = require('cheerio');
 var request = require('sync-request');
 var fs = require('fs');
+var file = 'movie.text';
 var url = 'http://movie.douban.com/subject/25724855/';
 var html = '';
 html = request('GET',url).getBody().toString();
 //console.log(html);
+var IMDBLink='';
 handleDB(html);
 function handleDB(html){
-    var $= cheerio.load(html);
+    var $ = cheerio.load(html);
     var info = $("#info");
     //获得电影名
-    var movieName = $("#content>h1>span").filter(function(i,el){
-        return $(this).attr('property') ==='v:itemreviewed';
+    var movieName = $("#content>h1>span").filter(function(i, el){
+        return $(this).attr('property') === 'v:itemreviewed';
     }).text();
     //获取影片导演名
-    var directories = '- 导演: ' + $("#info span a").filter(function(i,el){
+    var directories = '- 导演: ' + $("#info span a").filter(function(i, el){
             return $(this).attr('rel') === 'v:directedBy';
         }).text();
-    console.log(movieName+directories);
     //获取影片演员
     var starName = '- 主演: ';
-    $('.actor .attrs a').each(function(i,el){
-        starName += $(this).text()+'/';
+    $('.actor .attrs a').each(function(i, el){
+        starName += $(this).text() + '/';
     })
     //获取片长
-    var runTime = '- 片长: '+$("#info span").filter(function(i,el){
-            return $(this).attr('property')==='v:runtime';
+    var runTime = '- 片长: ' + $("#info span").filter(function(i, el){
+            return $(this).attr('property') === 'v:runtime';
         }).text();
     //获取影片类型
-    var kind = $("#info span").filter(function(i,el){
-        return $(this).attr("property")==='v:genre';
+    var kind = $("#info span").filter(function(i, el){
+        return $(this).attr("property") === 'v:genre';
     }).text();
+    //处理影片类型数据
+    var klength = kind.length;
+    var kinds = '- 影 片类型: ';
+    for(i=0;i<klength;i+=2){
+        kinds+=kind.slice(i,i+2)+'/';
+    }
+    //获取电影评分和电影评分人数
+    //豆瓣
+    var DBScore = $('.ll.rating_num').text();
+    var DBVotes = $('a.rating_people>span').text().replace(/\B(?=(\d{3})+$)/g,',');
+    var DB = '- 豆 瓣评分: ' + DBScore + '/10' + '('+DBVotes+'人评价)';
+    //IMDBLink
+    IMDBLink = $('#info').children().last().prev().attr('href');
+    var data = movieName +'\r\n'+directories +'\r\n'+starName+'\r\n'+runTime+'\r\n'+kinds+'\r\n'+DB+'\r\n';
+    // 输出文件
+    fs.appendFile(file,data,'utf-8',function(error){
+        if(error){
+            throw error;
+        }else {
+            console.log('大体信息写入完毕\r\n'+data);
 
+            var link = request('GET',IMDBLink).getBody().toString();
+            handleIMDB(link);
+        }
+    })
+}
+function handleIMDB(link){
+    var $ = cheerio.load(link);
+    //获取IMDB评分
+    var IMDBSore = $('.ratingValue span').filter(function(i,el){
+        return $(this).attr('itemprop')==='ratingValue';
+    }).text();
+    //获取IMDB评分人数
+    var IMDBVotes = $('.small').filter(function(i,el){
+        return $(this).attr('itemprop')==='ratingCount';
+    }).text();
+    //字符串拼接
+    var IMDB = '- IMDB评分: '+IMDBSore +'/10('+IMDBVotes+'人评价)\r\n';
+    //输出文件
+    fs.appendFile(file,IMDB,'utf-8',function(error){
+        if(error){
+            throw error
+        }else {
+            console.log('IMDB信息写入成功\r\n'+IMDB);
+        };
+    });
 }
